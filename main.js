8ff527715760dcb295ac96aa28464712f34b8c8d
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-12-22 19:02:07 +0100

    modified:   README.md 	modified:   index.html 	modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -31,3 +31,19 @@ $('.summer').owlCarousel({
         }
     }
 })
+
+var owl = $('.testimonials');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
