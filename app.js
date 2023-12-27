f18378478b37b039600135b6b2e1c6b9bc5cc555
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-12-27 11:36:42 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
